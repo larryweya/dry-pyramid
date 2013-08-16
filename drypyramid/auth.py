b4bfe3f4583472da65ec4ac2a47fd93614320246
Larry Weya <larryweya@gmail.com>
2013-08-16 15:43:20 +0300

    Function to do permission checks within templates

--- drypyramid/auth.py
+++ drypyramid/auth.py
@@ -1,3 +1,23 @@
 from passlib.context import CryptContext
+from pyramid.security import has_permission
+
 
 pwd_context = CryptContext()
+
+
+def permission_check_func(context, request):
+    """ Attach a function for has_permission checks within templates.
+
+    For example:
+
+    .. code-block:: python
+
+        @subscriber(BeforeRender)
+        def attach_has_permission_callback(event):
+        if event['view']:
+            event['has_permission'] = permission_check_func(
+                event['context'], event['request'])
+    """
+    def inner(permission):
+        return has_permission(permission, context, request)
+    return inner
