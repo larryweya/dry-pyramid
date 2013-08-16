4aacf40268196c9e4dbbe85b57476bd5e6b77a25
Larry Weya <larryweya@gmail.com>
2013-08-16 12:49:43 +0300

    Implemented TestBase

--- drypyramid/tests.py
+++ drypyramid/tests.py
@@ -1,6 +1,8 @@
 import unittest
-
+from pyramid import testing, traversal
+from pyramid.paster import bootstrap
 from sqlalchemy import (
+    create_engine,
     Column,
     Integer,
     Text,
@@ -12,7 +14,9 @@ from sqlalchemy import (
 from sqlalchemy.orm import (
     relationship,
 )
-from drypyramid.models import Base
+from drypyramid.models import (
+    SASession, Base, ModelFactory
+)
 
 
 person_hobby = Table(
@@ -24,6 +28,7 @@ person_hobby = Table(
 
 class Person(Base):
     __tablename__ = 'person'
+    __pluralized__ = 'people'
     id = Column(Integer, primary_key=True)
     name = Column(String(100), unique=True, nullable=False)
     age = Column(Integer, nullable=False)
@@ -32,20 +37,32 @@ class Person(Base):
 
 class Hobby(Base):
     __tablename__ = 'hobby'
+    __pluralized__ = 'hobbies'
     id = Column(Integer, primary_key=True)
     name = Column(String(100), unique=True, nullable=False)
 
 
-class TestBaseModel(unittest.TestCase):
+class PersonModelFactory(ModelFactory):
+    ModelClass = Person
+
+    def on_get_item(self, item):
+        self.on_get_item_called = True
+
+
+class TestBase(unittest.TestCase):
     def setUp(self):
-        super(TestBaseModel, self).setUp()
+        config = testing.setUp()
+        config.add_route('site', '/*traverse')
+        engine = create_engine('sqlite:///:memory:', echo=True)
+        SASession.configure(bind=engine)
+        Base.metadata.create_all(engine)
 
     def tearDown(self):
-        super(TestBaseModel, self).setUp()
+        SASession.remove()
+        testing.tearDown()
 
-    def test_something(self):
-        self.assertEqual(1, 1)
 
+class TestModel(TestBase):
     def test_create_from_dict(self):
         data = {
             'name': "Mr Smith",
@@ -77,3 +94,28 @@ class TestBaseModel(unittest.TestCase):
 
     def test_to_dict_handles_relationships(self):
         pass
+
+
+class TestModelFactory(TestBase):
+    def test_create_url(self):
+        request = testing.DummyRequest()
+        factory = PersonModelFactory(request)
+        url = factory.create_url(request)
+        expected_url = "%s/people/add" % request.application_url
+        self.assertEqual(url, expected_url)
+
+    def test_create_url_with_custom_action_name(self):
+        request = testing.DummyRequest()
+        factory = PersonModelFactory(request)
+        url = factory.create_url(request, 'create')
+        expected_url = "%s/people/create" % request.application_url
+        self.assertEqual(url, expected_url)
+
+    def test_get_item_calls_on_get_item(self):
+        request = testing.DummyRequest()
+        factory = PersonModelFactory(request)
+        # create a Person
+        person = Person(name="Mr Smith", age=23)
+        person.save()
+        factory.__getitem__('1')
+        self.assertTrue(factory.on_get_item_called)
